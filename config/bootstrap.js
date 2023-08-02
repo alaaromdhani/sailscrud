61596350e42294ce75a1a363ae766c4395dc9d30
ala.romdhani@esprit.tn <ala.romdhani@esprit.tn>
2023-08-02 11:48:26 +0000

    commit

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -57,7 +57,7 @@ module.exports.bootstrap = async function() {
   await matiereSeeders()
   await trimestresSeeders()
   await chaptersSeeders()
-  await documentSeeders()
+//  await documentSeeders()
  }
  catch(e){
   console.log(e)
