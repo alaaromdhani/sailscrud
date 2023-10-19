03a226bc7fb3b9e2186d43806f3a67148ccc025f
ala.romdhani@esprit.tn <ala.romdhani@esprit.tn>
2023-10-19 14:53:32 +0000

    ex-session

--- utils/constants.js
+++ utils/constants.js
@@ -1,20 +1,21 @@
 
-
 const databaseCredentials = {
-      user:'root',
-      password:'',
-  
-      options:{
-        port:'3306',
-        dialect:'mysql',
-        host:'localhost',
-      },
-      database:'madar',
-      baseUrl:'http://localhost:1337/',
-      allowedUrlOnCors: ['http://localhost:3000','https://madar-landing-page.vercel.app','http://localhost:4200', 'http://127.0.0.1:3000', 'http://127.0.0.1','http://127.0.0.1:3001','http://localhost:3001'],
-      internalEmailAddress: 'support@example.com',
-      registrationCode:'454568789742558',
-      frontUrl:'http://localhost:3000'
+  user:'madaruser',
+  password:'madar2023*',
+
+  options:{
+    port:'3306',
+    dialect:'mysql',
+    host:'localhost',
+            KeepAlive: 300000,
+            KeepAliveQuery: 'SELECT 1',
+  },
+  database:'madar',
+  baseUrl:'https://api.madar.tn/',
+  allowedUrlOnCors: ['https://madar-landing-page.vercel.app','https://www.madar.tn','https://madar.tn','https://www.office.madar.tn','https://office.madar.tn'],
+  internalEmailAddress: 'support@example.com',
+  registrationCode:'454568789742558',
+  frontUrl:'https://madar.tn'
 
 
 
